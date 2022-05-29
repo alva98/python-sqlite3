a465ccebb7ff5cfdc5c79f95ae363d553148cd9c
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2022-05-29 14:25:05 -0500

    Crea base de datos

--- conexion01.py
+++ conexion01.py
@@ -1,2 +1,13 @@
-import sqlite3
+import sqlite3 as sql3
 
+mi_db = "mi_empresa"
+
+conn =sql3.connect("database/"+ + mi_db +"sqlite")
+Cursor = conn.cursor()
+
+try:
+    Crea_Tabla()
+except Exception as ex:
+    print ("Error de excepción: " + e)
+
+Cursor.close()
