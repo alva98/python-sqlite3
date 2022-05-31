606695556ec5cfb99c63c6db43119a33a3509d3f
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2022-05-31 15:18:15 -0500

    Agregada función Crea_Tabla
    Se agrego la funcion para crear la tabla usuarios, con campos usuario y contraseña.

--- conexion01.py
+++ conexion01.py
@@ -1,7 +1,12 @@
 import sqlite3 as sql3
 
-mi_db = "mi_empresa"
+def Crea_Tabla():
+Cursor.execute("CREATE TABLE IF NOT EXISTS usuarios(usuarios(
+ varchar, contraseña varchar)")
+Cursor.commit()
+print("Tabla creada con exito.")
 
+mi_db = "mi_empresa"
 conn =sql3.connect("database/"+ + mi_db +"sqlite")
 Cursor = conn.cursor()
 
