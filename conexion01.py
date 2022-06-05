659a25bd11b17262e2c7718a93f42e4a2d451255
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2022-06-04 19:13:32 -0500

    Se crea función que crea la base se

--- conexion01.py
+++ conexion01.py
@@ -1,14 +1,25 @@
 import sqlite3 as sql3
 
+def Crea_DB():
+    try:
+        conn =sql3.connect("database/"+ mi_db + "sqlite")
+        conn =sql3.connect("database/"+ mi_db + "sqlite")
+        Mantén pulsado un clip para fijarlo. Los clips no fijados se eliminarán al cabo de una hora.conn =sql3.connect("database/"+ mi_db + "sqlite")
+conn =sql3.connect("database/"+ mi_db + "sqlite")
+Mantén pulsado un clip paa ijarlo. Los clips no fijados se eliminarán al cabo de una hh
+
 def Crea_Tabla():
-Cursor.execute("CREATE TABLE IF NOT EXISTS usuarios(usuarios(
- varchar, contraseña varchar)")
+
+Cursor.execute("CREATE TABLE IF NOT EXISTS usuarios(usuarios
+ varchar(50), contraseña varchar(50))")
 Cursor.commit()
 print("Tabla creada con exito.")
 
 mi_db = "mi_empresa"
-conn =sql3.connect("database/"+ + mi_db +"sqlite")
+conn =sql3.connect("database/"+ mi_db + "sqlite")
 Cursor = conn.cursor()
+
+def Crea_Tabla()
 
 try:
     Crea_Tabla()
