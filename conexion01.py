97fc12bd7a5b46857e76863d7ac354367dd868ce
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2022-06-04 19:57:53 -0500

    Corrección de edición de funciones.

--- conexion01.py
+++ conexion01.py
@@ -1,29 +1,22 @@
 import sqlite3 as sql3
+from sqlite3.dbapi2 import connect
 
 def Crea_DB():
     try:
         conn =sql3.connect("database/"+ mi_db + "sqlite")
-        conn =sql3.connect("database/"+ mi_db + "sqlite")
-        Mantén pulsado un clip para fijarlo. Los clips no fijados se eliminarán al cabo de una hora.conn =sql3.connect("database/"+ mi_db + "sqlite")
-conn =sql3.connect("database/"+ mi_db + "sqlite")
-Mantén pulsado un clip paa ijarlo. Los clips no fijados se eliminarán al cabo de una hh
+        print ("Base de datos creada.")
+    except sqlite3.error as error:
+        print("Error en sqlite3: ", error)
 
 def Crea_Tabla():
-
-Cursor.execute("CREATE TABLE IF NOT EXISTS usuarios(usuarios
- varchar(50), contraseña varchar(50))")
-Cursor.commit()
-print("Tabla creada con exito.")
+    Cursor.execute("CREATE TABLE IF NOT EXISTS usuarios(usuarios archar(50), contraseña varchar(50))")
+    cnne.commit()
+    print("Tabla creada con exito.")
 
 mi_db = "mi_empresa"
 conn =sql3.connect("database/"+ mi_db + "sqlite")
 Cursor = conn.cursor()
 
-def Crea_Tabla()
-
-try:
-    Crea_Tabla()
-except Exception as ex:
-    print ("Error de excepción: " + e)
 
 Cursor.close()
+conn.close ()
